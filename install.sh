081c1f9988d256552af0537b8f9c0b19d929310c
Motko <77466238+Motko222@users.noreply.github.com>
2025-10-08 22:00:52 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 cd $path
 
-[ -f env ] && cp env-sample env
+[ -f env ] || cp env-sample env
 nano env
 source $path/env
 

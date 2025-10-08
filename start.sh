9f81ce71acc72c55f269117efd6aa125cab206e4
Motko <77466238+Motko222@users.noreply.github.com>
2025-10-08 16:22:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,5 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
+
+/bin/bash -c "$(curl -fsSL https://depin.tashi.network/install.sh)" -

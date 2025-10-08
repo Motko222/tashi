0beb9f79c49669cca42574a05d4b0b69a29384bc
Motko <77466238+Motko222@users.noreply.github.com>
2025-10-08 16:21:54 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -3,5 +3,5 @@ path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
 
-cd $WORKDIR
-docker compose down
+docker stop $CONTAINER
+docker rm $CONTAINER

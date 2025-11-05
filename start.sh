1208640767540741a39e1db32d341d458e454689
Motko <77466238+Motko222@users.noreply.github.com>
2025-11-05 08:43:37 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,9 +6,11 @@ source $path/env
 docker stop $CONTAINER
 docker rm $CONTAINER
 
+#run original script
 #/bin/bash -c "$(curl -fsSL https://depin.tashi.network/install.sh)" -
 
-wget https://depin.tashi.network/install.sh script
+#run without interactions
+wget https://depin.tashi.network/install.sh -O script
 
 sed -i '/prompt/ { /choice/ c\
 choice=y

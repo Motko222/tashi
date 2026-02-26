d4b3981566c82f1371c522808834ea59bb5e56e0
Motko <77466238+Motko222@users.noreply.github.com>
2026-02-26 08:31:51 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
 folder=$(echo $path | awk -F/ '{print $NF}')
 source $path/env
+export TERM=dumb
 
 docker stop $CONTAINER
 docker rm $CONTAINER

3649a9bdd8c76a87e646869dbb5bb9817773f0af
deus3 <deusexm.9903@gmail.com>
2026-06-08 10:01:16 +0200

    report: define errors variable (was used but never set)

--- report.sh
+++ report.sh
@@ -9,6 +9,7 @@ cd $path
 
 version=$()
 docker_status=$(docker inspect $CONTAINER | jq -r .[].State.Status)
+errors=$(docker logs $CONTAINER --since 1h 2>&1 | grep -c -E "rror|ERR")
 
 bond=$(docker logs $CONTAINER | grep "resource node successfully bonded" | tail -1 | awk '{print $1}' | sed 's/\x1B\[[0-9;]*[A-Za-z]//g')
 diff=$(( $(date +%s) - $(date -d "$bond" +%s) ))
